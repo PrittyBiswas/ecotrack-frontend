92dd0ea33fed16374aaddcf8a01bcf23ca692772
PrittyBiswas <prittybiswas@gmail.com>
2025-11-14 13:45:25 +0530

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Challenges from "./pages/Challenges";
+import ChallengeDetail from "./pages/ChallengeDetail";
+import AddChallenge from "./pages/AddChallenge";
+import MyActivities from "./pages/MyActivities";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NotFound from "./pages/NotFound";
+import { ToastContainer } from "react-toastify";
+import EditChallenge from "./pages/EditChallenge";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/challenges" element={<Challenges />} />
+        <Route path="/challenges/:id" element={<ChallengeDetail />} />
+        <Route path="/challenges/add" element={<ProtectedRoute><AddChallenge /></ProtectedRoute>} />
+        <Route path="/my-activities" element={<ProtectedRoute><MyActivities /></ProtectedRoute>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route
+          path="/challenges/edit/:id"
+          element={
+        <ProtectedRoute>
+          <EditChallenge />
+        </ProtectedRoute>
+  }
+/>
 
-export default App
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+      <ToastContainer />
+    </>
+  );
+}
